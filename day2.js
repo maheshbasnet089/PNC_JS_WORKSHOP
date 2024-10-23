44c4cadd6366ecc309630cfae6dcb9b41bd5e49a
maheshbasnet089 <basnetmanish089@gmail.com>
2024-10-23 19:58:06 +0545

    first commit

---
+++ day2.js
@@ -0,0 +1,48 @@
+var cup = "coffee"
+var bottle = "paanii"
+var isNepali = true
+var age = 22  
+
+var shoe1 = "goldstar"
+var shoe2 = "umbro"
+var shoe3= "nike"
+
+var name1 = "Bob"
+var name2 = "Pranjal"
+var name2 = "Anup"
+
+//array 
+var shoes = ["goldstar","umbro","nike"]
+var names = ['Bob','Pranjal','Anup']
+
+
+var name = "Manish Basnet"
+var age = 22 
+var faculty = "BCA"
+var location = "Itahari"
+//object 
+var me = {
+    name : "Manish Basnet", 
+    age : 22, 
+    faculty : "BCA", 
+    location : "Itahari"
+}
+
+var haha = "Mac"
+var hehe = "Iphone"
+var huhu = "Airpods"
+
+var electronicsItems = ["Mac","Iphone","Airpods"]
+console.log(electronicsItems[0])
+var appleProduct = {
+    product1 : "Mac",
+    product2 : "Iphone",
+    product3 : "Airpods"
+}
+console.log(appleProduct.product3)
+
+var microsoftProduct = {
+
+}
+
+console.log(cup,age,isNepali)
