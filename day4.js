e70dbdfad961cfa1f7ac3a2b2406d1e83bb07568
maheshbasnet089 <basnetmanish089@gmail.com>
2024-10-24 19:31:50 +0545

    day 4

---
+++ day4.js
@@ -0,0 +1,101 @@
+
+
+// // condtional statements 
+// var isRaining = true 
+
+// // if(isRaining){
+// //     console.log("carry umbrella")
+// // }else{
+// //     console.log("Dont carry umbrella")
+// // }
+
+// //ternary operator  
+
+// // isRaining ? console.log("carry umbrella") : console.log("dont carry umbrella")
+
+// // 2>3 ? console.log("yes correct") : console.log("not correct")
+// // if(2>3){
+// //     console.log("yes correct")
+// // }else{
+// // //     console.log("not correct")
+
+    
+// // // }
+
+// // // short-circuit 
+// // isRaining && console.log("carry umbrella")
+
+// // // backend 
+// // // frontend - email 
+
+// // email && database ma lagerw halne code yaha hunchaa 
+// // if(email){
+// //     database ma lagerw halne code yaha hunchaa 
+// // }
+
+// var temperature = 2 
+
+// // tempature > 30 -- hot 
+// // temperature > 20 -- moderate
+// // else -- cold 
+
+// if(temperature > 30){
+//     console.log("its hot outside")
+// }else if(temperature > 20){
+//     console.log("its moderate outside")
+// }else{
+//     console.log("its cold outside")
+// }
+
+//loop 
+// for 
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday']
+
+// for loop for everything 
+// for of -- loop for array 
+
+for(let haha of days){
+    console.log(days)
+}
+
+// for in -- loop for object 
+
+// for, do while, while
+// calculator -- add feature - optimize
+// if --> while --> result --> heavy  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(days[0])
+// console.log(days[1])
+// console.log(days[2])
+// console.log(days[3])
+// for(var i=0;i < 10;i++){
+//     console.log(i)
+// }
+
+// for(var i = 0; i < days.length;i++){
+//     console.log(days[i])
+// }
+// i = 0, 0 < 4  T
+// i =1 , 1 < 4 T 
+// i =2, 2<4 T 
+// i =4 , 4 < 4 F 
+// i = 0 , 0 < 10 - T, 0, i = i + 1, i = 0 +1 = 1 
+
+// i = 1, 1 <10 - T, 
+//i =2 , 2 < 10 -T
+// i=3 , 3 < 10 -T 
+// i =9 , 9 <10 T 
+// i = 10 , 10<10 -F 
